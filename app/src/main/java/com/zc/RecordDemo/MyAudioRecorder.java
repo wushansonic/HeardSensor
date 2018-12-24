eafbabb80654edaa2bfa7d6e6d11e19802736e03
niuniu <bluefire.wu@dianping.com>
2018-12-24 17:30:50 +0800

    fix a bug

--- app/src/main/java/com/zc/RecordDemo/MyAudioRecorder.java
+++ app/src/main/java/com/zc/RecordDemo/MyAudioRecorder.java
@@ -144,16 +144,16 @@ public class MyAudioRecorder {
         mIsPause = false;
         mIsRecording = false;
 
-//        new Thread() {
-//            @Override
-//            public void run() {
-//                super.run();
-//                mEncodedFile = new File(path + "/" + AUDIO_FILENAME_PREFIX + "_tmp.mp3");
-//                mConveter.encodeFile(mRawFile.getAbsolutePath(), mEncodedFile.getAbsolutePath());
-//                mEncodedFile.renameTo(new File(path + "/" + AUDIO_MP3_FILENAME));
-//                mRawFile.delete();
-//            }
-//        }.start();
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                mEncodedFile = new File(path + "/" + AUDIO_FILENAME_PREFIX + "_tmp.mp3");
+                mConveter.encodeFile(mRawFile.getAbsolutePath(), mEncodedFile.getAbsolutePath());
+                mEncodedFile.renameTo(new File(path + "/" + AUDIO_MP3_FILENAME));
+                mRawFile.delete();
+            }
+        }.start();
 
     }
 
